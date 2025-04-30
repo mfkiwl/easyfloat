37963392105be10bd8dcaac90c6aff5d5d48bd72
Jiawei Lin <jiawei.lin@epfl.ch>
2025-04-30 16:39:32 +0200

    Added simple test cases

---
+++ tests/backend.py
@@ -0,0 +1,54 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from abc import ABC
+import pyeasyfloat.fma
+import pyeasyfloat.exp
+from pyeasyfloat.float import FloatPoint
+from pyverilator import PyVerilator
+
+class BaseFPBackend(ABC):
+    def fma(self, a: FloatPoint, b: FloatPoint, c: FloatPoint) -> FloatPoint:
+        """return a * b + c in precision of c"""
+        pass
+    
+    def exp2(self, x: FloatPoint, targetExpWidth: int, targetMantissaWidth: int) -> FloatPoint:
+        pass
+
+
+class PyEasyFloatBackend(BaseFPBackend):
+
+    def __init__(self):
+        super().__init__()
+
+    def fma(self, a: FloatPoint, b: FloatPoint, c: FloatPoint) -> FloatPoint:
+        """return a * b + c in precision of c"""
+        return pyeasyfloat.fma.fma(a, b, c, c.ew, c.mw)
+    
+    def exp2(self, x: FloatPoint, targetExpWidth: int, targetMantissaWidth: int) -> FloatPoint:
+        return pyeasyfloat.exp.pow2(x, targetExpWidth, targetMantissaWidth)
+
+class HwBackend(BaseFPBackend):
+
+    def __init__(self, svTopFile: str):
+        super().__init__()
+        self.sim = PyVerilator.build(svTopFile)
+    
+
+    def fma(self, a: FloatPoint, b: FloatPoint, c: FloatPoint) -> FloatPoint:
+        self.sim.io.io_in_exp2 = 0
+        self.sim.io.io_in_a = a.to_bits()
+        self.sim.io.io_in_b = b.to_bits()
+        self.sim.io.io_in_c = c.to_bits()
+        ret = self.sim.io.io_out
+        return FloatPoint.from_bits(ret, c.ew, c.mw)
+
+    def exp2(self, x: FloatPoint, targetExpWidth: int, targetMantissaWidth: int) -> FloatPoint:
+        self.sim.io.io_in_exp2 = 1
+        self.sim.io.io_in_a = x.to_bits()
+        self.sim.io.io_in_b = 0
+        self.sim.io.io_in_c = 0
+        ret = self.sim.io.io_out
+        # self.sim.clock.tick()
+        return FloatPoint.from_bits(ret, targetExpWidth, targetMantissaWidth)
---
+++ tests/fma_test.py
@@ -0,0 +1,61 @@
+from backend import *
+import subprocess
+import sys
+from pyeasyfloat.float import FloatPoint
+
+TEST_FLOAT = "../berkeley-testfloat-3/build/Linux-x86_64-GCC/testfloat_gen"
+TEST_FLAGS = "-tininessafter -exact -rnear_even"
+
+def fma_test_builder(ew: int, mw: int, backend: BaseFPBackend, n: int=-1, seed: int=0, level=1):
+    match (ew, mw):
+        case (8, 23):
+            fmt = 'f32'
+        case (5, 10):
+            fmt = 'f16'
+        case _:
+            raise ValueError(f"Unsupported format E={ew} M={mw}")
+    test_cmd = f"{TEST_FLOAT} {TEST_FLAGS} -seed {seed} -level {level} {fmt}_mulAdd"
+
+    process = subprocess.Popen(test_cmd,
+                               stdout=subprocess.PIPE,
+                               stderr=sys.stderr,
+                               shell=True,
+                               text=True)
+    try:
+        cnt = 0
+        skipped = 0
+        flushed = 0
+
+        for line in process.stdout:
+            a, b, c, ref, flags = [int(x, base=16) for x in line.split()]
+            a, b, c, ref = (FloatPoint.from_bits(x, ew, mw) for x in [a, b, c, ref])
+            if a.is_subnormal or b.is_subnormal or c.is_subnormal:
+                skipped += 1
+                continue
+            cnt += 1
+            dut: FloatPoint = backend.fma(a, b, c)
+            if ref.is_subnormal or (flags & 0b00010):
+                ref.exp = 0
+                ref.mantissa = 0
+                flushed += 1
+            if dut.to_bits() != ref.to_bits():
+                print(f"Test {cnt} failed:")
+                print(f"Input: {line}")
+                print(f"a : {a}")
+                print(f"b : {b}")
+                print(f"c : {c}")
+                print(f"-------------")
+                print(f"ref: {ref}")
+                print(f"-------------")
+                print(f"dut: {dut}")
+                break
+            if cnt == n:
+                break
+    finally:
+        process.stdout.close()
+        process.wait()
+        print(f"Test finished! tested:: {cnt} skipped: {skipped} flushed: {flushed}")
+            
+            
+
+fma_test_builder(8, 23, HwBackend('MulAddExp2.sv'), n=0)
